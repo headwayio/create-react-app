be531cbd8fe8ff59560f4ea71a04490cadf26b00
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2019-06-13 08:29:07 -0500

    Adds headway messaging

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -6,18 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Headway CRA Template</h1>
+        <subtitle>🌊 Let's make waves! 🌊</subtitle>
       </header>
     </div>
   );
